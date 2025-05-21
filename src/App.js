58c7d5f85d39f14ad107670b8677b76dfb02fe20
kyuiazbek <kyuiazbekchoibekov@gmail.com>
2025-05-21 17:21:01 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Layout from "./components/layout/Layout";
+import MainRoutes from "./routes/MainRoutes";
 
 const App = () => {
   return (
     <div>
-      
+      <MainRoutes />
     </div>
   );
 };
